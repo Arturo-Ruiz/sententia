e7353edc672f5778b5aa4156870a1c4eeffb6a7b
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2026-05-14 12:38:48 -0400

    Add Sentence model and migration for sentences table

---
+++ database/migrations/2026_05_14_163807_create_sentences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sentences', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('case_number')->unique()->index(); 
+            $table->string('url')->unique(); 
+            $table->string('court')->index();
+            $table->longText('content'); 
+            $table->jsonb('metadata')->nullable(); 
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sentences');
+    }
+};
